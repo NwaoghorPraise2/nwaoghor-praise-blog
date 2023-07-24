8cabc6e6af54c80aa847d4f1d49d29c0c20338d5
NwaoghorPraise2 <chukunwaoghor@gmail.com>
2023-07-24 15:07:13 +0100

    Update

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,43 @@
 import React from 'react'
+import Link from 'next/link'
+
+const link = [
+  {
+    id: 1,
+    title:"Home", 
+    url: "/"
+  },
+  {
+    id: 2,
+    title:"Portfolio", 
+    url: "/portfolio"
+  },
+  {
+    id: 3,
+    title:"Blog", 
+    url: "/blog"
+  },
+  {
+    id: 4,
+    title:"About", 
+    url: "/about"
+  },
+  {
+    id: 5,
+    title:"Contact", 
+    url: "/contact"
+  },
+  {
+    id: 6,
+    title:"Dashboard", 
+    url: "/dashboard"
+  }
+]
 
 function Navbar() {
   return (
     <div>
-      Navbar
+     <Link href="./">Nwaoghor-Praise</Link>
     </div>
   )
 }
